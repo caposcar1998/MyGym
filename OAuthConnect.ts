b62e7cbba336319db156d57ccf5865735a3524f9
caposcar1998 <oscarcontrerascap@gmail.com>
2021-09-07 13:03:59 -0500

    Se quitan cables

--- OAuthConnect.ts
+++
@@ -1,5 +0,0 @@
-export default class OAuthConnect{
-    static oauth_client = '174425546157-4uvq479eagvt5si2b34qrk68j099naku.apps.googleusercontent.com';
-    static oauth_secret = 'KQrX_w9sH3VUOLpCJx6FXGKU';
-}
-
