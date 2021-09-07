78f1f6518db17d726d4f0b45f31cc8693f39e63c
caposcar1998 <oscarcontrerascap@gmail.com>
2021-09-07 12:56:54 -0500

    Se arregla inicio de la base de datos

--- api/models/index.js
+++ api/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+let counter = 0
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -21,8 +21,24 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    if (counter == 0 ) {
+      const model = require(path.join(__dirname, "Usuarios.js"))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+      counter = counter + 1
+    } else if (counter == 1  ) {
+        const model = require(path.join(__dirname, "Ejercicios.js"))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+        counter = counter + 1
+    } else if (counter == 2){
+        const model = require(path.join(__dirname, "Rutinas.js"))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+        counter = counter + 1
+    } else if( counter ==3){
+      const model = require(path.join(__dirname, "EjerciciosRutinas.js"))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    }
+
+
   });
 
 Object.keys(db).forEach(modelName => {
