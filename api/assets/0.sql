d96738549f262f6a9e239648b57e45074e70edef
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2021-11-17 00:10:01 -0600

    Update 0.sql

--- api/assets/0.sql
+++ api/assets/0.sql
@@ -1,4 +1,4 @@
 INSERT INTO
-    usuarios(nombre,correo,apellido,edad,tipoCuerpo,diasGym,horasGym,intensidad,objetivoCuerpo,foto,idAuth,createdAt,updatedAt)
+    Usuarios(nombre,correo,apellido,edad,tipoCuerpo,diasGym,horasGym,intensidad,objetivoCuerpo,foto,idAuth,createdAt,updatedAt)
 VALUES  
-    ("Demo","a@a.com","demo",23,"Mesomorfo",5,5,"alta","Tonificar","foto","1",'2021-10-19 00:00:00','2021-10-19 00:00:00')
+    ("Demo","a@a.com","demo",23,"Mesomorfo",5,5,"alta","Tonificar","foto","1",'2021-10-19 00:00:00','2021-10-19 00:00:00');
